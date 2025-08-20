4fd78d2175975f51ea85c9e21974bfb9314ef3cf
akanimo sam okure <akan.okure@gmail.com>
2025-08-20 16:23:59 +0100

    add environment configuration file

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import { ENV_VARS } from "./config/envVars.js";
+
 const app = express();
+const PORT = ENV_VARS.PORT;
 
 app.use("/api/v1/auth", authRoutes);
 
-app.listen(5000, () => {
-  console.log("Server is running at http://localhost:5000");
+app.listen(PORT, () => {
+  console.log("Server is running at http://localhost:" + PORT);
 });
